c8302a6e66fec972f6cebec1060096ecd8b2ac00
erezviz <erezviz@gmail.com>
2022-04-13 11:33:10 +0300

    first commit

---
+++ js/meme.service.js
@@ -0,0 +1,20 @@
+'use strict'
+const IMG_KEY = 'imagesDB'
+let gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+let gMeme = {
+    selecttedImgId: 5,
+    selecttedLineIdx: 0
+}
+const ImageCount = 18
+
+// function createImages(){
+//     gImgs = loadFromStorage(IMG_KEY)
+//     if(!gImgs){
+//         gImgs = new Array(ImageCount)
+//         gImgs.forEach(img=> {
+
+//         })
+
+
+//     }
+// }
